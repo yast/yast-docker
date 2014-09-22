5c074544a86728f11089aa1b116ffaa381c17828
Josef Reidinger <jreidinger@suse.cz>
2014-09-22 12:48:48 +0200

    fix previos commit

--- src/lib/ydocker/changes_dialog.rb
+++ src/lib/ydocker/changes_dialog.rb
@@ -90,7 +90,7 @@ module YDocker
 
     def changes_items
       changes = @container.changes
-      changes.reject do |change|
+      changes.reject! do |change|
         path = change["Path"]
         changes.any? do |change2|
           change["Path"] != change2["Path"] && change2["Path"].start_with?(change["Path"])
