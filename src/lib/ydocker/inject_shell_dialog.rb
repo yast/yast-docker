8bdd2bd8e3ac2247b64e0a4915fbb6382eb965bd
Eduardo Navarro <enavarro@suse.com>
2017-11-14 11:05:04 +0100

    Fix Style/SpaceInsideBrackets rubocop offense
    - Fix Style/SpaceInsideBrackets rubocop offense with --auto-correct
    option.

--- src/lib/ydocker/inject_shell_dialog.rb
+++ src/lib/ydocker/inject_shell_dialog.rb
@@ -93,7 +93,7 @@ module YDocker
       )
     end
 
-    SHELLS = [ "bash", "sh", "zsh", "csh" ].freeze
+    SHELLS = ["bash", "sh", "zsh", "csh"].freeze
     def proposed_shells
       SHELLS.map { |shell| Item(Id(shell), shell) }
     end
